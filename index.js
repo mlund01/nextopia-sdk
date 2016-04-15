2d97a9b9cc490d6b0ee6e0005bd57a2368bfaf5e
Max Lund <mlund@four51.com>
2016-04-15 10:43:39 -0500

    add ip, clean up a bit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-angular.module('nextopia-sdk')
+angular.module('nextopia-sdk', [])
     .factory("Nextopia", Nextopia);
 
-function Nextopia($resource, nextopia_client_id) {
+function Nextopia($resource, nextopia_client_id, $window, $q, $http) {
     var service = {
         Search: _search,
         GetMeta: _getMeta,
@@ -45,9 +45,23 @@ function Nextopia($resource, nextopia_client_id) {
         'related_searches'
     ];
 
+    function getIp() {
+        var deferred = $q.defer();
+        var json = 'http://ipv4.myexternalip.com/json';
+        $http.get(json).then(function(result) {
+            deferred.resolve(result.data.ip);
+        });
+        return deferred.promise;
+    }
     var global_params = {
-        client_id: nextopia_client_id //constant to be set at runtime
+        client_id: nextopia_client_id, //constant to be set at runtime,
+        user_agent: $window.navigator.userAgent
     };
+    getIp()
+        .then(function(data) {
+            global_params.ip = data;
+        });
+
 
     function _setGlobalParam(param, value) {
         if (possibleParams.indexOf(param) > -1 && ['string', 'number', 'boolean'].indexOf(typeof value) > -1) {
@@ -59,21 +73,34 @@ function Nextopia($resource, nextopia_client_id) {
 
     function _resetGlobalParams() {
         global_params = {
-            client_id: global_params.client_id
+            client_id: nextopia_client_id,
+            user_agent: $window.navigator.userAgent,
+            ip: global_params.ip
         }
     }
+
     function _removeGlobalParam(param) {
-        if (global_params[param]) {
+        if (global_params[param] && ['client_id', 'user_agent', 'ip'].indexOf(param) == -1) {
             delete global_params[param];
         }
     }
     function _getMeta() {
-        return $resource('http://ecommerce-search.nextopiasoftware.com/return-results.php?json=1&nav_search=1&res_per_page=1').get().$promise.then(function(data) {data.results ? delete data.results : angular.noop(); return data});
+        var params = {
+            client_id: nextopia_client_id,
+            user_agent: $window.navigator.userAgent,
+            ip: global_params.ip,
+            callback: "JSON_CALLBACK",
+            json: 1,
+            nav_search: 1,
+            res_per_page: 1
+        };
+        return $resource('http://ecommerce-search.nextopiasoftware.com/return-results.php?', params, {call: {method: "JSONP"}}).call().$promise.then(function(data) {data.results ? delete data.results : angular.noop(); return data});
     }
 
     function _search(custom_params) {
         var final_query = combineQueryObjects(custom_params,global_params);
-        return $resource('http://ecommerce-search.nextopiasoftware.com/return-results.php?json=1', final_query).get().$promise;
+        final_query.callback = "JSON_CALLBACK";
+        return $resource('http://ecommerce-search.nextopiasoftware.com/return-results.php?json=1', final_query, {call: {method: 'JSONP', headers: {"access-control-allow-origin": $window.location.origin}}}).call().$promise;
     }
 
     function combineQueryObjects(custom, global) {
@@ -85,4 +112,3 @@ function Nextopia($resource, nextopia_client_id) {
 
     return service;
 }
-
